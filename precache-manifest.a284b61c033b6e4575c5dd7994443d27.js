20eec00618dc3b492cbf4d56b223d6ef0688550f
Sinisa Milicic <shasini92@gmail.com>
2019-07-15 01:55:18 +0200

    Updates

---
+++ precache-manifest.a284b61c033b6e4575c5dd7994443d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9063ddf0d47bf650da3db8c81a9e4bf9",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "9a375284b1f5f9925995",
+    "url": "/ContactManager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "67a654cd37d6ac7dfa6d",
+    "url": "/ContactManager/static/css/main.9e346195.chunk.css"
+  },
+  {
+    "revision": "9a375284b1f5f9925995",
+    "url": "/ContactManager/static/js/2.97e825c8.chunk.js"
+  },
+  {
+    "revision": "67a654cd37d6ac7dfa6d",
+    "url": "/ContactManager/static/js/main.2da6bce5.chunk.js"
+  },
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  }
+]);
